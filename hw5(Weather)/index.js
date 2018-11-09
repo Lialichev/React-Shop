564cfc6929f32d2212204a47d32b33ed08c60c28
Vladislav Lyalichev <32138449+Lyalichev@users.noreply.github.com>
2018-11-09 11:31:29 +0200

    del comments for index

--- hw5(Weather)/index.js
+++ hw5(Weather)/index.js
@@ -1,8 +1,6 @@
-// Module
 const fs = require('file-system');
 const hp = require('http');
 const opn = require('opn');
-// Module End
 
 hp.get('http://wttr.in/~kharkov', (data) => {
     if (data.statusCode !== 200) throw new Error(data.statusMessage);
