8aacbed931142b722d19007cfc77fb79dcadcf85
Lyalichev <lyalichevv98@gmail.com>
2018-11-13 21:39:35 +0200

    class 13.11.2018

--- nodeServer/public/main.js
+++
@@ -1,5 +0,0 @@
-const images = document.querySelectorAll('img');
-
-images.forEach((e, i) => {
-    e.addEventListener('load', () => console.log(`Image(${i}) = Width x Height (${e.naturalWidth} x ${e.naturalHeight})`));
-});
---
+++ nodeServer/src/app.js
@@ -0,0 +1,6 @@
+const header = require('./components/header')();
+const footer = require('./components/footer')();
+const $ = require('jquery');
+
+document.body.appendChild(header);
+document.body.appendChild(footer);
