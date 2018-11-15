ce095540149c608996c965334540ab3f18d66dd8
Lyalichev <lyalichevv98@gmail.com>
2018-11-15 22:06:16 +0200

    webpack update

---
+++ nodeServer/configs/webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const package = require('../package.json');
+
+module.exports = {
+    // entry: './app.js',
+    entry: {
+        main: './app.js'
+    },
+    context: path.resolve(__dirname, '../src'),
+    output: {
+        filename: 'index.js',
+        path: path.resolve(__dirname, '../public')
+    },
+
+    mode: 'development',
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    },
+
+    plugins: [new HtmlWebpackPlugin({
+        title: package.name,
+        template: './index.html',
+        version: package.version
+    })],
+
+    optimization: {
+        splitChunks: {
+            chunks: 'all'
+        }
+    }
+};
