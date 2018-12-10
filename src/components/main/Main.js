930215c7a8de890f00f8e6d796492db7a937d056
Lyalichev <lyalichevv98@gmail.com>
2018-12-11 00:23:38 +0200

    Get posts

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,11 +1,15 @@
 import Aside from '../aside';
 import Content from '../content';
 import UserList from '../userList';
+import Posts from '../posts';
 
 import './main.scss';
 
 class Main extends Component {
-  state = {users: []};
+  state = {
+    users: [],
+    posts: false
+  };
 
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
@@ -13,19 +17,23 @@ class Main extends Component {
       .then(users => this.setState({users}))
   }
 
-  showUserName(user) {
-    alert(user.username);
+  showUserName({ id }) {
+    fetch(`https://jsonplaceholder.typicode.com/posts?userId=${id}`)
+      .then(res => res.json())
+      .then(posts => this.setState({posts}))
   }
 
   render() {
     const {title} = this.props;
-    const {users} = this.state;
+    const {users, posts} = this.state;
+    console.log(posts);
 
     return (
       <main className="main" title={title}>
         <Aside/>
         <Content/>
-        <UserList items={users} handleClick={this.showUserName}/>
+        <UserList items={users} handleClick={(e) => this.showUserName(e)}/>
+        <Posts posts={posts} />
       </main>
     );
   }
