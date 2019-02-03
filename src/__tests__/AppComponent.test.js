e67fa1c70fd599ef1ba3a6b43acb62381eeff716
Lyalichev <lyalichevv98@gmail.com>
2019-02-03 17:27:09 +0200

    Finished test AppComponent

--- src/__tests__/AppComponent.test.js
+++ src/__tests__/AppComponent.test.js
@@ -1,8 +1,10 @@
-import { shallow } from 'enzyme'
-import { AppComponent } from '../AppComponent'
-import { getInfo, cleanInfo } from 'store/category'
+import { shallow } from 'enzyme';
+import { AppComponent } from '../AppComponent';
+import { getInfo, cleanInfo } from 'store/category';
+import { logout } from 'store/user'
+import { cleanError } from 'store/status'
 
-describe('<AppComponent />', () => {
+describe('<AppComponent', () => {
   it('should match to snapshot', () => {
     const wrapper = shallow(<AppComponent dispatch={_ => _} />);
     expect(wrapper).toMatchSnapshot();
@@ -20,7 +22,36 @@ describe('<AppComponent />', () => {
     const user = {};
 
     wrapper.setProps({ user });
-
     expect(fakeDispatch).toHaveBeenCalledWith(getInfo());
+  });
+
+  it('should send action cleanInfo() if user changed', () => {
+    const fakeDispatch = jest.fn();
+    const history = { push: jest.fn() };
+    const user = { name: 'John' };
+    const wrapper = shallow(<AppComponent user={user} history={history} dispatch={fakeDispatch} />);
+
+    wrapper.setProps({ user: null });
+    expect(fakeDispatch).toHaveBeenCalledWith(cleanInfo());
+  });
+
+  it('should show error and send action cleanError() after show error', () => {
+    const fakeDispatch = jest.fn();
+    const wrapper = shallow(<AppComponent dispatch={fakeDispatch} />);
+    const instance = wrapper.instance();
+    instance.container = { error: jest.fn() };
+    const error = 'Some error';
+
+    wrapper.setProps({ error });
+    expect(fakeDispatch).toHaveBeenCalledWith(cleanError());
+  });
+
+  it('should send action logout() if user logout', () => {
+    const fakeDispatch = jest.fn();
+    const wrapper = shallow(<AppComponent dispatch={fakeDispatch} />);
+    const instance = wrapper.instance();
+
+    instance.onLogout();
+    expect(fakeDispatch).toHaveBeenCalledWith(logout());
   });
 });
